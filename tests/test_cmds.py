0358a32251426c86c77c19c64fe86cc7cfacf767
enen92 <92enen@gmail.com>
2019-12-05 15:41:47 +0000

    Update tests

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
+import sys
 from resources.lib.tubecast.youtube.utils import parse_cmd
 
 cmd_1 = '[[20,["remoteDisconnected",{"app":"android-phone-13.05.52","pairingType":"dial","capabilities":"atp,que,mus","ui":"true","clientName":"android","experiments":"","name":"MYPHONE","remoteControllerUrl":"ws://","id":"something","type":"REMOTE_CONTROL","device":"{\"app\":\"android-phone-13.05.52\",\"pairingType\":\"dial\",\"capabilities\":\"atp,que,mus\",\"clientName\":\"android\",\"experiments\":\"\",\"name\":\"MYPHONE\",\"remoteControllerUrl\":\"ws://\",\"id\":\"id\",\"type\":\"REMOTE_CONTROL\"}"}]]'
 cmd_2 = '20,["remoteDisconnected",{"app":"android-phone-13.05.52","pairingType":"dial","capabilities":"atp,que,mus","ui":"true","clientName":"android","experiments":"","name":"MYPHONE","remoteControllerUrl":"ws://","id":"something","type":"REMOTE_CONTROL","device":"{\"app\":\"android-phone-13.05.52\",\"pairingType\":\"dial\",\"capabilities\":\"atp,que,mus\",\"clientName\":\"android\",\"experiments\":\"\",\"name\":\"MYPHONE\",\"remoteControllerUrl\":\"ws://\",\"id\":\"id\",\"type\":\"REMOTE_CONTROL\"}"}]]'
 
 
+PY3 = sys.version_info.major >=3
+
+
+def str_to_bytes(string):
+    if PY3:
+        return bytes(string, 'utf-8')
+    return string
+
+
 def test_cmd1():
-    _, data = parse_cmd(cmd_1)
+    _, data = parse_cmd(str_to_bytes(cmd_1))
     assert data["name"] == "MYPHONE"
 
 
 def test_cmd2():
-    _, data = parse_cmd(cmd_2)
+    _, data = parse_cmd(str_to_bytes(cmd_2))
     assert data["name"] == "MYPHONE"
