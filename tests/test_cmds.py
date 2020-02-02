55a90e1464ce773d0706f7f1a350a45cbb6a9d95
enen92 <92enen@gmail.com>
2020-02-02 23:25:37 +0000

    Fix tests and command parser

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
-import sys
 from resources.lib.tubecast.youtube.utils import CommandParser
 
 cmd_1 = '[[20,["remoteDisconnected",{"app":"android-phone-13.05.52","pairingType":"dial","capabilities":"atp,que,mus","ui":"true","clientName":"android","experiments":"","name":"MYPHONE","remoteControllerUrl":"ws://","id":"something","type":"REMOTE_CONTROL","device":"{\"app\":\"android-phone-13.05.52\",\"pairingType\":\"dial\",\"capabilities\":\"atp,que,mus\",\"clientName\":\"android\",\"experiments\":\"\",\"name\":\"MYPHONE\",\"remoteControllerUrl\":\"ws://\",\"id\":\"id\",\"type\":\"REMOTE_CONTROL\"}"}]]'
 cmd_2 = '20,["remoteDisconnected",{"app":"android-phone-13.05.52","pairingType":"dial","capabilities":"atp,que,mus","ui":"true","clientName":"android","experiments":"","name":"MYPHONE","remoteControllerUrl":"ws://","id":"something","type":"REMOTE_CONTROL","device":"{\"app\":\"android-phone-13.05.52\",\"pairingType\":\"dial\",\"capabilities\":\"atp,que,mus\",\"clientName\":\"android\",\"experiments\":\"\",\"name\":\"MYPHONE\",\"remoteControllerUrl\":\"ws://\",\"id\":\"id\",\"type\":\"REMOTE_CONTROL\"}"}]]'
 
 
-PY3 = sys.version_info.major >=3
-
-
-def str_to_bytes(string):
-    if PY3:
-        return bytes(string, 'utf-8')
-    return string
-
-
 def test_cmd1():
-    cmd = next(iter(CommandParser(str_to_bytes(cmd_1))))
-    assert cmd.code == 20
-    assert cmd.name == "remoteDisconnected"
-    assert cmd.data["name"] == "MYPHONE"
+    for cmd in CommandParser(cmd_1):
+        assert cmd.code == 20
+        assert cmd.name == "remoteDisconnected"
+        assert cmd.data["name"] == "MYPHONE"
 
 
 def test_cmd2():
-    cmd = next(iter(CommandParser(str_to_bytes(cmd_2))))
-    assert cmd.code == 20
-    assert cmd.name == "remoteDisconnected"
-    assert cmd.data["name"] == "MYPHONE"
+    for cmd in CommandParser(cmd_2):
+        assert cmd.code == 20
+        assert cmd.name == "remoteDisconnected"
+        assert cmd.data["name"] == "MYPHONE"
