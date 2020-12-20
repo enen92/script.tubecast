82b1f9286f87895752226a33bf482f7cdf9ba644
enen92 <92enen@gmail.com>
2020-12-20 17:30:30 +0000

    Use named branches

--- main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from resources.lib import service
-from resources.lib.kodi import kodilogging
-
-
-kodilogging.config()
-service.run()
--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import os.path
-import sys
-
-
-def pytest_configure():
-    sys.path.insert(0, os.path.abspath("tests/xbmc_mock"))
